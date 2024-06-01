29ae16f9d57596df8580cf879c75e75ff34e7409
venkatdeepak28 <tvenkatadeepak@gmail.com>
2024-06-01 12:47:58 +0530

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (request, response) => {
+app.get('/gadgets', (request, response) => {
   response.sendFile('./gadgets.html', {root: __dirname})
 })
 
